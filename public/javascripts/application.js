95d69207edbad3b1966bfec72e687f8228bbbd61
Craig Protzel <craigprotzel@gmail.com>
2013-07-12 19:02:11 -0400

    Started redesigning the site

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,13 +6,29 @@ var respondVideo = function(){
 };
 
 
+var randomSort = function(){return 0.5 - Math.random();};
+
 
 $( document ).ready( function(){
 
 	//Randomly select a video
-	var currentVid = loadVids[Math.floor(Math.random() * loadVids.length)];
-	$('#embedVideo').html(currentVid);
-	//Make the video responsive
+	//var currentVid = loadVids[Math.floor(Math.random() * loadVids.length)];
+
+	loadVids.sort(randomSort);
+
+	var videoValues = [];
+	for (var i = 0; i < loadVids.length; i++){
+		videoValues.push(i);
+	}
+	videoValues.sort(randomSort);
+	videoValues.sort(randomSort);
+	videoValues.sort(randomSort);
+	console.log(videoValues);
+
+	$('#embedVideoOne').html(loadVids[videoValues[0]]);
+	$('#embedVideoTwo').html(loadVids[videoValues[1]]);
+	$('#embedVideoThree').html(loadVids[videoValues[2]]);
+
 	respondVideo();
 	$(window).resize(function() {
 		respondVideo();
